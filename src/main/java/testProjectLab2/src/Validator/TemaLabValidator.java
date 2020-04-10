79814744172ffdd37fabbe46e9915736d37389a1
catadascalu <catadascalu@yahoo.com>
2020-04-10 12:49:04 +0300

    added tests for all invalid cases - add assignment

--- src/main/java/testProjectLab2/src/Validator/TemaLabValidator.java
+++ src/main/java/testProjectLab2/src/Validator/TemaLabValidator.java
@@ -6,11 +6,11 @@ import testProjectLab2.src.Exceptions.ValidatorException;
 public class TemaLabValidator implements IValidator<TemaLab> {
 
     public void validate(TemaLab t) throws ValidatorException {
-        if(t.getId()== null || t.getId() == -1){
+        if(t.getId()== null || t.getId() < 0){
             throw new ValidatorException("Nr tema invalid\n");
         }else if(t.getDescriere()== null || t.getDescriere().equals("")) {
             throw new ValidatorException("Descriere tema invalida\n");
-        }else if(t.getSaptammanaPredarii()<=0 || t.getSaptammanaPredarii()>14) {
+        }else if(t.getSaptamanaPredarii()<=0 || t.getSaptamanaPredarii()>14) {
             throw new ValidatorException("Saptamana predarii invalida\n");
         }else if(t.getTermenLimita()<=0 || t.getTermenLimita()>14) {
             throw new ValidatorException("Termen limita invalid\n");
