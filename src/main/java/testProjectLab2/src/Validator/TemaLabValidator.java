2eeedcdacd82f7e13afeea09624951e81f11a2bf
catadascalu <catadascalu@yahoo.com>
2020-04-10 12:59:26 +0300

    added tests for all invalid cases - add assignment

--- src/main/java/testProjectLab2/src/Validator/TemaLabValidator.java
+++ src/main/java/testProjectLab2/src/Validator/TemaLabValidator.java
@@ -6,7 +6,7 @@ import testProjectLab2.src.Exceptions.ValidatorException;
 public class TemaLabValidator implements IValidator<TemaLab> {
 
     public void validate(TemaLab t) throws ValidatorException {
-        if(t.getId()== null || t.getId() < 0){
+        if(t.getId()== null || t.getId() <= 0){
             throw new ValidatorException("Nr tema invalid\n");
         }else if(t.getDescriere()== null || t.getDescriere().equals("")) {
             throw new ValidatorException("Descriere tema invalida\n");
